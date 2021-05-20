eb0c1bc456882226fbeea69e1ad4e73ff5d7174e
thakaj <taulant.hakaj@scranton.edu>
2021-05-20 14:09:30 -0400

    commit tree 0e392e7a5cfbba2b51b76950cd5b5d99d4ff6043

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(input) {
+    if (input >= 42){
+        return (input - 42);
+    }else {
+        return (42 -input)
+    }
+}
+function distanceFromHqInFeet(input){
+    if (input >= 42){
+        return ((input -42)*264)
+    }else {
+        return ((42- input)*264)
+    }
+}
+function distanceTravelledInFeet(input1, input2){
+    if (input1 >= input2){
+        return ((input1 - input2)*264)
+    }else {
+        return ((input2 - input1)* 264)
+    }
+}
+function calculatesFarePrice (input1, input2){
+    if (distanceTravelledInFeet(input1,input2) <= 400){
+        return 0;
+    }else if (distanceTravelledInFeet(input1,input2) >400 && distanceTravelledInFeet(input1,input2) < 2000){
+        return ((distanceTravelledInFeet(input1,input2) -400)*.02);
+    }else if (distanceTravelledInFeet(input1,input2) >= 2000 && distanceTravelledInFeet(input1,input2) <= 2500){
+        return 25;
+    }else if (distanceTravelledInFeet(input1,input2) > 2500){
+        return 'cannot travel that far';
+    }
+}
